784b35830493821c4d2c6b914a8e8dce64ab62d4
Megalonia <moosman863@gmail.com>
2021-05-20 19:29:55 -0500

    add start to post-service

---
+++ post-service/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{App,HttpServer};
+use dotenv::dotenv;
+
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let pool = create_connection_pool();
+    run_migrations(&pool);
+
+    let schema = create_schema_with_context(pool);
+
+    HttpServer::new(move || App::new().configure(configure_service).data(schema.clone()))
+        .bind("0.0.0.0:8001")?
+        .run()
+        .await
+}
+
